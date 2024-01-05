2ed4eadd42095669cae65972357d7c6cf141ed6c
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-05 11:53:07 +0100

    one component

--- components/ButtonArea.js
+++ components/ButtonArea.js
@@ -2,10 +2,26 @@ export const ButtonArea = () => {
 
 	setTimeout(() => {
 		const btnColorElem = document.querySelector('.btn-color');
+		const btnTextColorElem = document.querySelector('.btn-text-color');
+		const messageElem = document.querySelector('.message');
+
+		messageElem.style.color = 'red';
+		messageElem.style.backgroundColor = 'lightblue';
+		messageElem.style.width = '20rem';
+		messageElem.style.padding = '1rem';
+		messageElem.style.borderRadius = '.5rem';
+		messageElem.style.border = '2px solid black';
+		messageElem.style.boxShadow = '5px 5px 1px #ff0000, 10px 10px 1px #0000ff';
+		messageElem.style.fontSize = "3rem";
+		messageElem.innerHTML = "Hello <b> World </b> ";
 
 		btnColorElem.addEventListener("click", () => {
 			btnColorElem.style.backgroundColor = "red";
 			btnColorElem.style.color = "yellow";
+		});
+
+		btnTextColorElem.addEventListener("click", () => {
+			messageElem.style.color = "blue";
 		});
 	}, 0);
 
--- main.js
+++ main.js
@@ -5,21 +5,3 @@ document.querySelector('#app').innerHTML = `
 	<p class="message">original message</p>
 	${ButtonArea()}
 `;
-
-const messageElem = document.querySelector('.message');
-const btnTextColorElem = document.querySelector('.btn-text-color');
-
-messageElem.style.color = 'red';
-messageElem.style.backgroundColor = 'lightblue';
-messageElem.style.width = '20rem';
-messageElem.style.padding = '1rem';
-messageElem.style.borderRadius = '.5rem';
-messageElem.style.border = '2px solid black';
-messageElem.style.boxShadow = '5px 5px 1px #ff0000, 10px 10px 1px #0000ff';
-messageElem.style.fontSize = "3rem";
-messageElem.innerHTML = "Hello <b> World </b> ";
-
-// event
-btnTextColorElem.addEventListener("click", () => {
-	messageElem.style.color = "blue";
-});
